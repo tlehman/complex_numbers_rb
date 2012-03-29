6e60e29a6c7eb99738ade578dafb756ad675d7bf
Tobi Lehman <tobi.lehman@gmail.com>
2012-03-28 18:02:58 -0700

    First commit. Using RSpec, 6 Tests passed and 1 Failed

---
+++ README.md
@@ -0,0 +1,23 @@
+Complex Number object in Ruby
+=============================
+
+I know that there is already a Complex object baked into ruby's standard 
+library, but the point here is to learn and apply test-driven development.
+
+Writing tests before any actual code has been written is a new idea to me, 
+actually, my first response to it was a mix of confusion and anger, but 
+after more reading, watching screencasts, writing tests for existing code, 
+and finally using testing to drive the development of new features, I am 
+starting to appreciate the methodology.
+
+This is just the beginning, but so far I have used TDD to drive the 
+development of 
+
+	* raising a ComplexNumber to a power (n >= 0)
+	* dividing a ComplexNumber by another (nonzero) ComplexNumber
+
+Also, using TDD, I was able to modify the exponentiation and division methods 
+to allow for cases when the argument was zero. I noticed that starting with 
+the tests kept me more focused on the desired behavior, and the red/green 
+coloring was a good visual reminder of what it was I was working on.
+
---
+++ complex_number.rb
@@ -0,0 +1,54 @@
+class ComplexNumber
+  attr_accessor :re, :im
+
+  def initialize(re, im)
+    # when ComplexNumber.new is called, initialize 
+    # is run after the object is instantiated
+    @re = re
+    @im = im
+  end
+
+  def +(z)
+    ComplexNumber.new @re + z.re, @im + z.im
+  end
+
+  def *(z)
+		z = ComplexNumber.new(z, 0) unless z.is_a? ComplexNumber
+    ComplexNumber.new @re*z.re - @im*z.im, @im*z.re + @re*z.im
+  end
+
+  def conjugate
+    # complex conjugate
+    ComplexNumber.new @re, -@im
+  end
+
+	def ^(n)
+		nth_pow = ComplexNumber.new 1, 0
+		raise StandardError unless n.is_a?(Integer) || n<0
+		n.times do |i| 
+			nth_pow = self*nth_pow
+		end
+		nth_pow
+	end
+
+	def /(z)
+		coef = (z.im**2 + z.re**2)
+		div_re = (@im*z.im + @re*z.re)/coef
+		div_im = (-@re*z.im + @im*z.re)/coef
+		ComplexNumber.new div_re, div_im
+	end
+
+  def to_s
+    # the to_s function outputs a string automatically, for example:
+    # suppose z is an object of type ComplexNumber, then   
+    # puts z     will implicitly call z.to_s and then output the result
+    output = ""
+    if @im >= 0
+      output = "#{@re} + #{@im}i"
+    else
+      output = "#{@re} - #{-1*@im}i"
+    end 
+    output # return the string
+  end
+end
+
